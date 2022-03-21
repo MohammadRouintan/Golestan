a5d22c7f61a23e14a4201b4e24e336cc0239ebe5
Mohammad Rouintan <mdrn1381@gmail.com>
2022-03-21 08:39:08 +0330

    Create StudentAccount.java

---
+++ src/main/java/com/example/golestan/Account/StudentAccount.java
@@ -0,0 +1,72 @@
+package com.example.golestan.Account;
+
+import com.example.golestan.Database.StudentDB;
+import javafx.scene.control.Alert;
+
+import java.sql.SQLException;
+
+public class StudentAccount extends StudentDB {
+
+    public StudentAccount() {
+        super();
+    }
+
+    public boolean login(String username, String password, String job) throws SQLException {
+        super.setUsername(username);
+        super.setPassword(password);
+        if (job == null || !job.equals("Student")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("PLEASE SELECT CORRECT JOB !!");
+            alert.show();
+            return false;
+        }
+
+        if (!super.checkStuWithUsername()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("USER NOT FOUND !!\nPLEASE ENTER CORRECT USERNAME OR PASSWORD.");
+            alert.show();
+        } else {
+            while (super.read().next()) {
+                String recievePassword = super.read().getString("Password");
+                if (recievePassword.equals(password)) {
+                    super.disconnect();
+                    return true;
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("USER NOT FOUND !!\nPLEASE ENTER CORRECT PASSWORD.");
+                    alert.show();
+                }
+            }
+        }
+
+        super.disconnect();
+        return false;
+    }
+
+    public boolean signup(int studentId, String username, String password, String firstName, String lastName, String majorSubject, String college, int enteringYear) throws SQLException {
+        super.setStudentId(studentId);
+        super.setUsername(username);
+        super.setPassword(password);
+        super.setFirstName(firstName);
+        super.setLastName(lastName);
+        super.setMajorSubject(majorSubject);
+        super.setCollege(college);
+        super.setEnteringYear(enteringYear);
+
+        if (super.checkStuWithUsername()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("USER ALREADY EXIST.\nYOU CANNOT USE THIS USERNAME !!");
+            alert.show();
+            return false;
+        } else if (super.checkStuWithStudentId()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("USER ALREADY EXIST.\nYOU CANNOT USE THIS STUDENT ID !!");
+            alert.show();
+            return false;
+        } else {
+            super.addStu();
+        }
+
+        return true;
+    }
+}
