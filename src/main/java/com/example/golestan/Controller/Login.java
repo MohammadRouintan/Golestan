650c363a522aef676d58a5065861650e4982b3cf
Mohammad Rouintan <mdrn1381@gmail.com>
2022-03-21 16:27:54 +0330

    Create login form

---
+++ src/main/java/com/example/golestan/Controller/Login.java
@@ -0,0 +1,45 @@
+package com.example.golestan.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private ChoiceBox<?> jobInput;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordInput;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    private TextField usernameInput;
+
+    @FXML
+    void loginClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quitClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signupClicked(ActionEvent event) {
+
+    }
+
+}
